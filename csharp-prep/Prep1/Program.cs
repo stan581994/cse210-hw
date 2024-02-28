3deffb1fa9ea672fd03d9ca66e27bc206b18c158
stan581994 <stan581994@gmail.com>
2024-02-28 20:59:41 +0800

    prep1 and prep 2 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {firstName}, {firstName} {lastName}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade? ");
+        String userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        String letterGrade;
+
+        if(grade >= 90){
+            letterGrade = "A";
+        } 
+        else if(grade >= 80){
+            letterGrade = "B";
+        }
+        else if(grade >= 70){
+           letterGrade = "c";
+            
+        }
+        else if(grade >= 60){
+            letterGrade = "D";
+        }
+        else{
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your Letter Grade is {letterGrade}");
+        if(grade == 70){
+            Console.Write("Is the user passed? [y] or [n] ");
+            String isItPassed = Console.ReadLine();
+            if(isItPassed == "y"){
+                Console.WriteLine("Congratulations, You are passed!"); 
+            }else{
+                Console.WriteLine("It's okay. Keep doing better next time!"); 
+            }
+        }
+
+
     }
 }
