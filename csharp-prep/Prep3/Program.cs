3cf581f0f6fbc201fa36c564f6453c3efa9e9fb7
stan581994 <stan581994@gmail.com>
2024-02-28 21:11:34 +0800

    done prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+    
+        int magicNumber = rnd.Next(20);
+        bool continueLoop = true;
+
+        do{
+            Console.Write("What is your guess? ");
+            int guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber == magicNumber){
+                Console.Write("You guessed it!");
+                continueLoop = false;
+            }
+            else if (guessNumber > magicNumber){
+                Console.WriteLine("Lower");
+            }else{
+                Console.WriteLine("Higher");
+            }
+        }while(continueLoop);
     }
 }
