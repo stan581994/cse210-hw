74d2e9f0d949aca4e570597e5be141141b848fb2
stan581994 <stan581994@gmail.com>
2024-03-14 15:27:19 +0800

    added conditional loop

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,16 +8,24 @@ class Program
         Reference reference = new Reference("Proverbs",3,5,6);
         Scripture scripture = new Scripture(reference,verse);
         string response = "";
+        bool hideFewWords = false;
 
         do {
 
-            Console.Clear();
+            if(hideFewWords){
+                scripture.hideRandomWords(3);
+            }
+
+          //  Console.Clear();
             Console.WriteLine(scripture.getDisplayText());
             Console.WriteLine(" ");
             Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
             response = Console.ReadLine();
+            hideFewWords = true;
 
-        }while(response != "quit");
+
+
+        }while(response != "quit" || scripture.isCompletelyHidden());
        
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,6 +3,10 @@ public class Scripture{
     private Reference reference;
     private List<Word> words = new List<Word>();
 
+    private bool _isCompletelyHidden = false;
+
+    private int sizeOfAllHiddenWords = 0;
+
     public Scripture(Reference reference, string text){
         this.reference = reference;
         String[] verseWords = text.Split(" ");
@@ -14,19 +18,46 @@ public class Scripture{
     }
 
     public void hideRandomWords(int numbersToHide){
+        Random random = new Random();
+        int randomIndex = 0;
+
+        for (int i = 0; i<3; i++){
+
+            do{
+                Console.WriteLine($"{sizeOfAllHiddenWords} == {words.Count-1}");
+                if (sizeOfAllHiddenWords  == (words.Count - 1)){
+                    _isCompletelyHidden = true;
+                    break;
+                }
+                randomIndex =  random.Next(0, words.Count);
+                if(words[randomIndex].isHidden()){
+                    
+                }
+            }while( || sizeOfAllHiddenWords  == (words.Count - 1));
+            sizeOfAllHiddenWords++;
+            words[randomIndex].hide();
+        }
+
 
     }
 
     public string getDisplayText(){
         StringBuilder stringBuilder = new StringBuilder();
         foreach(Word word in words){
-            stringBuilder.Append(word.getDisplayText());
-            stringBuilder.Append(" ");
+            if(word.isHidden()){
+                string replacement = new string('_', word.getDisplayText().Length);
+                stringBuilder.Append(word.getDisplayText().Replace(word.getDisplayText(),replacement));
+                stringBuilder.Append(" ");
+            }else{
+                stringBuilder.Append(word.getDisplayText());
+                stringBuilder.Append(" ");
+            }
+
         }
         return $"{reference.getDisplayText()} {stringBuilder.ToString()}";
     }
 
     public bool isCompletelyHidden(){
-        return false;
+        return _isCompletelyHidden;
     }
 }
