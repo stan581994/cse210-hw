02ddf95ebba1a3a4d017baa84c72cfe07ef574f1
stan581994 <stan581994@gmail.com>
2024-04-06 23:55:27 +0800

    initial commit for final project

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+       RentalServiceManager rentalServiceManager = new RentalServiceManager();
+       rentalServiceManager.run();
     }
 }
